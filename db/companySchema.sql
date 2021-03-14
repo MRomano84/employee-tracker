6f358bca5e85c3e3921eba197e77a466c31c120a
Matthew Romano <mathiasrok@gmail.com>
2021-03-14 12:44:50 -0400

    clean up / foreign keys added

--- db/companySchema.sql
+++ db/companySchema.sql
@@ -4,24 +4,24 @@ CREATE DATABASE company_db;
 USE company_db;
 
 CREATE TABLE employee(
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
-    PRIMARY KEY (id)
-);
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+)
 
 CREATE TABLE role(
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
-    PRIMARY KEY (id)
-);
+    FOREIGN KEY (department_id) REFERENCES department(id)
+)
 
 CREATE TABLE department(
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30),
-    PRIMARY KEY (id)
-);
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+)
