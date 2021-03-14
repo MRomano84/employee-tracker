2239eebafb1f65565fb354601df68650f25b0b7d
Matthew Romano <mathiasrok@gmail.com>
2021-03-14 16:10:33 -0400

    function to add emps

--- app.js
+++ app.js
@@ -23,8 +23,14 @@ connection.connect((err) => {
     // console.log(`${connection.threadId} is connected on PORT# ${connection.port}`);
 })
 
+//-----Functions to view all Employees, Departments, and Roles
 const viewEmps = () => {
-    connection.query('SELECT first_name, last_name, role_id FROM employee;',
+    console.log(
+        chalk.whiteBright(
+            figlet.textSync('EMPLOYEES', { horizontalLayout: 'controlled smushing', font: 'slant' }) //CYBERLARGE MERLIN1 SHADOW SLANT
+        )
+    );
+    connection.query('SELECT first_name, last_name, role_id Department FROM employee JOIN role ON employee.role_id = role.department_id',
     function (err, res) {
         if (err) throw (err);
         console.table(res);
@@ -33,7 +39,7 @@ const viewEmps = () => {
 }
 
 const viewDepts = () => {
-    connection.query('SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;',
+    connection.query('SELECT department.name FROM department',
     function (err, res) {
         if (err) throw (err);
         console.table(res);
@@ -43,17 +49,87 @@ const viewDepts = () => {
 
 const viewRoles = () => {
     connection.query('SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;',
-    function (err, res, fields) {
+    function (err, res) {
         if (err) throw (err);
         console.table(res);
         start();
     })
 }
 
+//-----Functions to add Employees, Departments, and Roles
+const addEmp = () => {
+    console.log(
+        chalk.whiteBright(
+            figlet.textSync('ADD\nEMPLOYEE', { horizontalLayout: 'controlled smushing', font: 'slant' }) //CYBERLARGE MERLIN1 SHADOW SLANT
+        )
+    );
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: 'Enter First Name:'
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: 'Enter Last Name:'
+        },
+        {
+            type: 'input',
+            name: 'role_id',
+            message: 'Enter the Role ID:'
+        },
+        {
+            type: 'input',
+            name: 'manager_id',
+            message: 'Enter the Manager\'s ID:'
+        },
+    ])
+    .then((answers) => {
+        connection.query('INSERT INTO employee SET ?',
+        {
+            first_name: answers.first_name,
+            last_name: answers.last_name,
+            role_id: answers.role_id,
+            manager_id: answers.manager_id,
+        },
+        function(err, res) {
+            if (err) throw (err);
+            console.table(res.answers);
+            start();
+        })
+    })
+}
+
+const addDept = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter The Name of The New Department'
+        }
+    ])
+    .then((answers) => {
+        connection.query('INSERT INTO department SET ?',
+        {
+            name: answers.name
+        },
+        function(err, res) {
+            if (err) throw (err);
+            console.table(department);
+            start();
+        })
+    })
+}
+
+const addRole = () => {
+    
+}
+
 const start = () => {
     console.log(
         chalk.cyan(
-            figlet.textSync('EMPLOYEE TRACKER', { horizontalLayout: 'full', font: 'CYBERLARGE' }) //CYBERLARGE MERLIN1 SHADOW SLANT
+            figlet.textSync('EMPLOYEE\nTRACKER', { horizontalLayout: 'full', font: 'CYBERLARGE' }) //CYBERLARGE MERLIN1 SHADOW SLANT
         )
     );
     inquirer.prompt({
@@ -85,10 +161,10 @@ const start = () => {
                 viewRoles(); 
                 break;
             case 'Add Employee':
-                //FUNCTION TO ADD EMP
+                addEmp();
                 break;
             case 'Add Department':
-                //FUNCTION TO ADD DEPT 
+                addDept();
                 break;
             case 'Add Roles':
                 //FUNCTION TO ADD ROLES 
